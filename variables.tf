0ff4b9d7b31ffd0921d855e99c883f965abbe3d6
Warren Uniewski <warren@autimo.com>
2023-02-13 15:08:46 -0800

    add gha workflows

--- variables.tf
+++ variables.tf
@@ -1,47 +1,47 @@
 
 variable "project_definition_file_path" {
-	default = "project.json"
-	description = "Full path to JSON file containing project definition structure. (project.json by convention)"
-	type = string
+  default     = "project.json"
+  description = "Full path to JSON file containing project definition structure. (project.json by convention)"
+  type        = string
 }
 
 variable "organization" {
-	type = string
-	description = "The name of the Terraform Cloud / Enterprise organization in which the workspace and other resources should be created."
+  type        = string
+  description = "The name of the Terraform Cloud / Enterprise organization in which the workspace and other resources should be created."
 }
 
 variable "tfc_token" {
-	type = string
-	description = "Token with administrative access to Terraform cloud organization targeted by the module."
+  type        = string
+  description = "Token with administrative access to Terraform cloud organization targeted by the module."
 }
 
 variable "team_access_level" {
-	type = string
-	default = "plan"
-	description = "The value indicating the level of access (permission set) that the TF Cloud team being created should have in the workspace being created."
+  type        = string
+  default     = "plan"
+  description = "The value indicating the level of access (permission set) that the TF Cloud team being created should have in the workspace being created."
 }
 
 variable "secret_access_key" {
-	description = "An AWS secret access key."
-	type = string
-	default = null
+  description = "An AWS secret access key."
+  type        = string
+  default     = null
 }
 
 variable "access_key_id" {
-	description = "An AWS access key id."
-	type = string
-	default = null
+  description = "An AWS access key id."
+  type        = string
+  default     = null
 }
 
 variable "execution_mode" {
-	description = "Terraform 'execution mode' - either 'remote' or 'local'.  See TFC docs for description of each."
-	type = string
-	default = "remote"
+  description = "Terraform 'execution mode' - either 'remote' or 'local'.  See TFC docs for description of each."
+  type        = string
+  default     = "remote"
 }
 
 variable "project_accounts" {
-	description = "The set of AWS project accounts for which we want to create TFC workspaces."
-	type = any
-	default = null
+  description = "The set of AWS project accounts for which we want to create TFC workspaces."
+  type        = any
+  default     = null
 }
 
